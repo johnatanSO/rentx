78a877c59e13375d95acb0f14f6219a7f570915e
johnatanSO <jonystrit@gmail.com>
2023-10-05 17:20:28 -0300

    :sparkles: [FEAT] - Filtros de carros.

--- front/src/components/screens/Home/Filters/index.tsx
+++ front/src/components/screens/Home/Filters/index.tsx
@@ -1,3 +1,39 @@
+import { useEffect, useState, FormEvent } from 'react'
+import style from './Filters.module.scss'
+import { TextField, MenuItem } from '@mui/material'
+import { Category } from '../interfaces/Category'
+import { getAllCategoriesService } from '@/services/category/GetAllCategoriesService'
+
 export function Filters() {
-  return <></>
+  const [categories, setCategories] = useState<Category[]>([])
+
+  function onFilterCars(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+  }
+
+  async function getAllCategories() {
+    const { data } = await getAllCategoriesService()
+
+    setCategories(data.items)
+  }
+
+  useEffect(() => {
+    getAllCategories()
+  }, [])
+
+  return (
+    <form className={style.filtersContainer} onSubmit={onFilterCars}>
+      <TextField size="small" label="Nome" type="text" />
+      <TextField size="small" label="Categoria" select>
+        {categories.map((category) => {
+          return (
+            <MenuItem key={category._id} value={category._id}>
+              {category.name}
+            </MenuItem>
+          )
+        })}
+      </TextField>
+      <button type="submit">Filtrar</button>
+    </form>
+  )
 }
