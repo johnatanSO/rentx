95a9db5ff2867fb61747bd81878c18fbb12f2854
johnatanSO <jonystrit@gmail.com>
2023-10-09 17:21:56 -0300

    :sparkles [FEAT] - Criando modal. :

---
+++ front/src/components/_ui/ModalLayout/index.tsx
@@ -0,0 +1,58 @@
+import { faXmark } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Modal, Box } from '@mui/material'
+import { ReactNode, FormEvent } from 'react'
+import style from './ModalLayout.module.scss'
+import { Loading } from '../Loading'
+
+interface Props {
+  title: string
+  open: boolean
+  submitButtonText?: string
+  children: ReactNode
+  handleClose: () => void
+  onSubmit?: (event: FormEvent<HTMLFormElement>) => void
+  loading?: boolean
+  customStyle?: any
+}
+export function ModalLayout({
+  title,
+  open,
+  submitButtonText,
+  handleClose,
+  children,
+  onSubmit,
+  loading,
+  customStyle,
+}: Props) {
+  return (
+    <Modal className={style.overlay} open={open} onClose={handleClose}>
+      <Box
+        style={customStyle ?? customStyle}
+        className={style.container}
+        onSubmit={onSubmit || (() => undefined)}
+        component="form"
+      >
+        <Box className={style.header} component="header">
+          <h3>{title || 'Modal'}</h3>
+          <FontAwesomeIcon
+            onClick={handleClose}
+            className={style.closeButton}
+            icon={faXmark}
+          />
+        </Box>
+
+        <Box className={style.mainContent} component="main">
+          {children}
+        </Box>
+        {onSubmit && (
+          <Box className={style.footer} component="footer">
+            <button disabled={loading} type="submit">
+              {loading ? <Loading /> : submitButtonText || 'Confirmar'}
+            </button>
+          </Box>
+        )}
+      </Box>
+    </Modal>
+  )
+}
