aa621590223433f231c0cdf0112ee28d77038d4b
johnatanSO <jonystrit@gmail.com>
2023-10-09 11:22:49 -0300

    :sparkles: [FEAT] - Definindo imagem default para carros que não possuem nenhuma imagem.

--- front/src/components/screens/Home/ListCars/CarItem/index.tsx
+++ front/src/components/screens/Home/ListCars/CarItem/index.tsx
@@ -1,7 +1,10 @@
 import Image from 'next/image'
+import style from './CarItem.module.scss'
+import { CarImage } from '../../interfaces/CarImage'
+import unknownCarImage from '../../../../../../public/assets/images/cars/unknownCarImage.png'
 
 type Props = {
-  images: any[]
+  images: CarImage[]
   name: string
 }
 
@@ -13,15 +16,28 @@ export function CarItem({ images, name }: Props) {
 
   return (
     <li>
-      {images.map((image) => {
-        return (
-          <Image
-            key={image._id}
-            alt="Imagem do carro"
-            src={getImageUrl(image.path)}
-          />
-        )
-      })}
+      {images.length === 0 ? (
+        <Image
+          className={style.carImage}
+          width={512}
+          height={512}
+          src={unknownCarImage}
+          alt="Carro sem imagem"
+        />
+      ) : (
+        images.map((image) => {
+          return (
+            <Image
+              className={style.carImage}
+              width={512}
+              height={512}
+              key={image._id}
+              alt="Imagem do carro"
+              src={getImageUrl(image.path)}
+            />
+          )
+        })
+      )}
       <h4>{name || '--'}</h4>
     </li>
   )
---
+++ front/src/components/screens/Home/interfaces/CarImage.ts
@@ -0,0 +1,6 @@
+export interface CarImage {
+  _id: string
+  imageName: string
+  carId: string
+  path: string
+}
