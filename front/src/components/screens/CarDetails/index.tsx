11a4a3e4eef8893951689cc22f3fb3f6b53a9111
johnatanSO <jonystrit@gmail.com>
2023-10-09 14:33:48 -0300

    :sparkles: [FEAT] - Criando layout do detalhes de carro.

--- front/src/components/screens/CarDetails/index.tsx
+++ front/src/components/screens/CarDetails/index.tsx
@@ -1,6 +1,10 @@
 'use client'
 import { useRouter } from 'next/navigation'
 import { Car } from '../Home/interfaces/Car'
+import style from './CarDetails.module.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleLeft } from '@fortawesome/free-solid-svg-icons'
+import Image from 'next/image'
 
 type Props = {
   car: Car
@@ -9,17 +13,39 @@ type Props = {
 export function CarDetails({ car }: Props) {
   const router = useRouter()
   return (
-    <>
-      <button
-        type="button"
-        onClick={() => {
-          router.back()
-        }}
-      >
-        Voltar
-      </button>
-      <h1>{car.name}</h1>
-      <span>{car.dailyRate}</span>
-    </>
+    <section className={style.carDetailsContainer}>
+      <header className={style.titleContainer}>
+        <button
+          type="button"
+          onClick={() => {
+            router.back()
+          }}
+          title="Voltar"
+        >
+          <FontAwesomeIcon className={style.icon} icon={faAngleLeft} />
+        </button>
+        <h2>{car.name}</h2>
+      </header>
+
+      <main>
+        <div className={style.carImageContainer}>
+          <Image
+            className={style.carImage}
+            width={500}
+            height={600}
+            src={''}
+            alt="Imagem do carro"
+          />
+        </div>
+
+        <div className={style.infosContainer}>
+          - Categoria - Especificações - Descrição - Valor - Placa - Data
+          inicial/Data final - Descrição - Alugar
+          <button className={style.rentalButton} type="button">
+            Alugar
+          </button>
+        </div>
+      </main>
+    </section>
   )
 }
