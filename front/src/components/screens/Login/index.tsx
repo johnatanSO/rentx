57ff95049014c4bfa06211c7d93ecc09c2e7c880
johnatanSO <jonystrit@gmail.com>
2023-10-16 11:36:22 -0300

    :lipstick: [STYLE] - Estilizando página de login.

--- front/src/components/screens/Login/index.tsx
+++ front/src/components/screens/Login/index.tsx
@@ -1,3 +1,24 @@
+'use client'
+
+import { FormEvent } from 'react'
+import style from './Login.module.scss'
+
 export function Login() {
-  return <h1> Realizar Log in</h1>
+  async function onAuthenticate(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+  }
+
+  return (
+    <section className={style.loginContainer}>
+      <form onSubmit={onAuthenticate}>
+        <header>Entrar com uma conta existente</header>
+
+        <main></main>
+
+        <footer>
+          <button type="submit">Entrar</button>
+        </footer>
+      </form>
+    </section>
+  )
 }
