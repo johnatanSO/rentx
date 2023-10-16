4d637236cb0b8334fa523210990913a2457029bb
johnatanSO <jonystrit@gmail.com>
2023-10-16 14:11:30 -0300

    :sparkles: [FEAT] - Criando página de cadastro de nova conta.

--- front/src/components/screens/CreateAccount/index.tsx
+++ front/src/components/screens/CreateAccount/index.tsx
@@ -1,3 +1,36 @@
+'use client'
+
+import style from './CreateAccount.module.scss'
+import { FormEvent } from 'react'
+import { CustomTextField } from '@/components/_ui/CustomTextField'
+import Link from 'next/link'
+
 export function CreateAccount() {
-  return <h1> Criar conta</h1>
+  async function onRegister(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+  }
+
+  return (
+    <section className={style.loginContainer}>
+      <form onSubmit={onRegister}>
+        <header>
+          <h4>Criar nova conta</h4>
+        </header>
+
+        <main>
+          <CustomTextField className={style.input} label="E-mail" />
+          <CustomTextField className={style.input} label="Nome" />
+          <CustomTextField className={style.input} label="Senha" />
+          <CustomTextField className={style.input} label="Confirmar senha" />
+        </main>
+
+        <footer>
+          <button type="submit">Entrar</button>
+          <Link className={style.createAccountLink} href="/authenticate">
+            Entrar com conta existente
+          </Link>
+        </footer>
+      </form>
+    </section>
+  )
 }
--- front/src/components/screens/Login/index.tsx
+++ front/src/components/screens/Login/index.tsx
@@ -1,7 +1,9 @@
 'use client'
 
-import { FormEvent } from 'react'
 import style from './Login.module.scss'
+import { FormEvent } from 'react'
+import { CustomTextField } from '@/components/_ui/CustomTextField'
+import Link from 'next/link'
 
 export function Login() {
   async function onAuthenticate(event: FormEvent<HTMLFormElement>) {
@@ -11,12 +13,20 @@ export function Login() {
   return (
     <section className={style.loginContainer}>
       <form onSubmit={onAuthenticate}>
-        <header>Entrar com uma conta existente</header>
+        <header>
+          <h4>Entrar</h4>
+        </header>
 
-        <main></main>
+        <main>
+          <CustomTextField className={style.input} label="E-mail" />
+          <CustomTextField className={style.input} label="Senha" />
+        </main>
 
         <footer>
           <button type="submit">Entrar</button>
+          <Link className={style.createAccountLink} href="/register">
+            Criar conta
+          </Link>
         </footer>
       </form>
     </section>
