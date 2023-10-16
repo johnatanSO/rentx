400c5051fa2fd6d0a39571e8372f31a71f47c8d4
johnatanSO <jonystrit@gmail.com>
2023-10-16 14:39:18 -0300

    :sparkles: [FEAT] - Manipulando valores dos iputs.

--- front/src/components/screens/CreateAccount/index.tsx
+++ front/src/components/screens/CreateAccount/index.tsx
@@ -1,11 +1,24 @@
 'use client'
 
 import style from './CreateAccount.module.scss'
-import { FormEvent } from 'react'
+import { FormEvent, useState } from 'react'
 import { CustomTextField } from '@/components/_ui/CustomTextField'
 import Link from 'next/link'
+import { Checkbox, FormControlLabel } from '@mui/material'
+import { NewUser } from './interfaces/NewUser'
 
 export function CreateAccount() {
+  const defaultValuesNewUser = {
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+    driverLicense: '',
+    isAdmin: false,
+  }
+
+  const [newUserData, setNewUserData] = useState<NewUser>(defaultValuesNewUser)
+
   async function onRegister(event: FormEvent<HTMLFormElement>) {
     event.preventDefault()
   }
@@ -18,10 +31,90 @@ export function CreateAccount() {
         </header>
 
         <main>
-          <CustomTextField className={style.input} label="E-mail" />
-          <CustomTextField className={style.input} label="Nome" />
-          <CustomTextField className={style.input} label="Senha" />
-          <CustomTextField className={style.input} label="Confirmar senha" />
+          <CustomTextField
+            label="Nome"
+            required
+            className={style.input}
+            value={newUserData.name}
+            onChange={(event) => {
+              setNewUserData({
+                ...newUserData,
+                name: event?.target.value,
+              })
+            }}
+          />
+          <CustomTextField
+            label="E-mail"
+            required
+            className={style.input}
+            value={newUserData.email}
+            onChange={(event) => {
+              setNewUserData({
+                ...newUserData,
+                email: event?.target.value,
+              })
+            }}
+          />
+          <CustomTextField
+            label="Senha"
+            required
+            className={style.input}
+            value={newUserData.password}
+            onChange={(event) => {
+              setNewUserData({
+                ...newUserData,
+                password: event?.target.value,
+              })
+            }}
+          />
+          <CustomTextField
+            label="Confirmar senha"
+            required
+            className={style.input}
+            value={newUserData.confirmPassword}
+            onChange={(event) => {
+              setNewUserData({
+                ...newUserData,
+                confirmPassword: event?.target.value,
+              })
+            }}
+          />
+          <CustomTextField
+            label="Nº da carteira"
+            required
+            className={style.input}
+            value={newUserData.driverLicense}
+            onChange={(event) => {
+              setNewUserData({
+                ...newUserData,
+                driverLicense: event?.target.value,
+              })
+            }}
+          />
+          <FormControlLabel
+            label={
+              <div className={style.labelCheckboxContainer}>
+                <span>Conta de administrador</span>
+                <span className={style.helperText}>
+                  *Somente para fins de testes
+                </span>
+              </div>
+            }
+            onChange={(event: any) => {
+              setNewUserData({
+                ...newUserData,
+                isAdmin: event?.target.checked,
+              })
+            }}
+            control={
+              <Checkbox
+                sx={{
+                  '&.Mui-checked': { color: '#536d88' },
+                }}
+                checked={newUserData.isAdmin}
+              />
+            }
+          />
         </main>
 
         <footer>
---
+++ front/src/components/screens/CreateAccount/interfaces/NewUser.ts
@@ -0,0 +1,8 @@
+export interface NewUser {
+  name: string
+  email: string
+  password: string
+  confirmPassword: string
+  driverLicense: string
+  isAdmin: boolean
+}
