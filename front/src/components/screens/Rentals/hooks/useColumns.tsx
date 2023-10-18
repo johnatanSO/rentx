a02005f2ac9a95574db3a63df876004396d9afa2
johnatanSO <jonystrit@gmail.com>
2023-10-18 17:06:18 -0300

    :sparkles: [FEAT] - Rentals list.

--- front/src/components/screens/Rentals/hooks/useColumns.tsx
+++ front/src/components/screens/Rentals/hooks/useColumns.tsx
@@ -1,33 +1,12 @@
 import { CellFunctionParams } from '@/components/_ui/TableComponent/interfaces'
 import dayjs from 'dayjs'
-import Image from 'next/image'
-import unknownCarImage from '../../../../../public/assets/images/cars/unknownCarImage.png'
-import { CarImage } from '../interfaces/CarImage'
 
 export function useColumns() {
-  function getCarIcon(images: CarImage[]) {
-    if (images.length === 0) return unknownCarImage
-
-    return process.env.NEXT_PUBLIC_END_POINT + images[0].path
-  }
-
   return [
     {
-      headerName: '',
-      field: 'car',
-      cellRenderer: (params: CellFunctionParams) => (
-        <Image
-          width={60}
-          height={60}
-          alt="Car default image"
-          src={getCarIcon(params.value.images)}
-        />
-      ),
-    },
-    {
       headerName: 'Carro',
-      field: 'car',
-      valueFormatter: (params: CellFunctionParams) => params.value.name,
+      field: 'carId',
+      valueFormatter: (params: CellFunctionParams) => params?.value || '--',
     },
     {
       headerName: 'Inicio',
