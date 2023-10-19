9d0a14b136bcf1cc15c7d011a1e0f17332c9de91
johnatanSO <jonystrit@gmail.com>
2023-10-19 15:47:10 -0300

    :lock: [SECURITY] - Verificando se usuário tem permissão de administrador para acessar a página de gestão.

--- front/src/app/management/page.tsx
+++ front/src/app/management/page.tsx
@@ -1,5 +1,10 @@
 import { HomePageManagement } from '@/components/screens/Management'
+import { getLocalUserService } from '@/services/user/getLocalUser/GetLocalUserService'
+import { redirect } from 'next/navigation'
 
 export default async function ManagementPage() {
+  const userHasAdminPermission = (await getLocalUserService()).isAdmin
+
+  if (!userHasAdminPermission) redirect('/')
   return <HomePageManagement />
 }
