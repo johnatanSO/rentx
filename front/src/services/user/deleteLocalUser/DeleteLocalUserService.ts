d139d1ce3cb7d08b0050a25e47e5b1974ff52ed8
johnatanSO <jonystrit@gmail.com>
2023-10-19 13:47:13 -0300

    :sparkles: [FEAT] - Setting delete cookies.

--- front/src/services/user/deleteLocalUser/DeleteLocalUserService.ts
+++ front/src/services/user/deleteLocalUser/DeleteLocalUserService.ts
@@ -2,7 +2,7 @@
 import { cookies } from 'next/headers'
 const USER_KEY = ':user: [INFO]'
 
-export function deleteLocalUserService() {
+export async function deleteLocalUserService() {
   globalThis?.localStorage?.removeItem(USER_KEY)
   cookies().delete(USER_KEY)
 }
