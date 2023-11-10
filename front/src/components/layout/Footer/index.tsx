c4cfd0e193a7ae0950d9b1ac3ffdbece77191362
johnatanSO <jonystrit@gmail.com>
2023-11-10 15:14:46 -0300

    :bookmark: [VERSION] - Exibindo tag de versão no footer.

--- front/src/components/layout/Footer/index.tsx
+++ front/src/components/layout/Footer/index.tsx
@@ -1,9 +1,10 @@
 import style from './Footer.module.scss'
+import packageJSON from '../../../../package.json'
 
 export function Footer() {
   return (
     <footer className={style.footerContainer}>
-      <span>Footer</span>
+      <span>&copy; Rentx - Johnatan v {packageJSON.version}</span>
     </footer>
   )
 }
