8aad131595bc287db1bfcb6565d0b9f6f5e41416
johnatanSO <jonystrit@gmail.com>
2023-11-22 15:34:10 -0300

    :sparkles: [FEAT] - Formatando requisição para update de informações do usuário.

--- front/src/services/cars/updateCarInfos/UpdateCarInfosService.ts
+++ front/src/services/cars/updateCarInfos/UpdateCarInfosService.ts
@@ -3,13 +3,14 @@ import http from '@/http/axios'
 interface IRequest {
   _id: string
   name: string
+  description: string
 }
 
-export function updateCarInfosService({ name, _id }: IRequest) {
+export function updateCarInfosService({ _id, name, description }: IRequest) {
   const body = {
-    _id,
     name,
+    description,
   }
 
-  return http.put('/cars/', body)
+  return http.put(`/cars/${_id}`, body)
 }
