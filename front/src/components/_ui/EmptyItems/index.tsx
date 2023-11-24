12db361349cf4f089acbedbdd89e442fa55f54db
johnatanSO <jonystrit@gmail.com>
2023-11-24 15:47:43 -0300

    :sparkles: [FEAT] - Creating emptyItems.

---
+++ front/src/components/_ui/EmptyItems/index.tsx
@@ -0,0 +1,22 @@
+import { faExclamationCircle } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import style from './EmptyItems.module.scss'
+import { IconProp } from '@fortawesome/fontawesome-svg-core'
+
+type Props = {
+  text: string
+  icon?: IconProp
+  customStyle?: any
+}
+
+export function EmptyItems({ customStyle, text, icon }: Props) {
+  return (
+    <div className={style.container} style={customStyle ?? customStyle}>
+      <FontAwesomeIcon
+        className={style.icon}
+        icon={icon || faExclamationCircle}
+      />
+      {text || 'Nenhum item encontrado'}
+    </div>
+  )
+}
