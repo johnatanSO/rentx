8881b29a1d95d1ce67f931d018cfd1c4f9f3e024
johnatanSO <jonystrit@gmail.com>
2023-12-07 13:52:11 -0300

    :wrench: [CONFIGS] - Update URL configs images.

--- front/next.config.js
+++ front/next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['http://localhost:3333', 'localhost'],
+    domains: [
+      'http://localhost:3333',
+      'localhost',
+      'https://rentx-webservice.onrender.com',
+    ],
   },
   experimental: {
     serverActions: true,
