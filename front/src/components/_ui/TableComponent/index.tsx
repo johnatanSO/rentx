a12142c41ed752d5f41ffc040e8868135cc4e51e
johnatanSO <jonystrit@gmail.com>
2023-12-19 15:09:59 -0300

    :iphone: [MOBILD] - Deixando o footer responsivo.

--- front/src/components/_ui/TableComponent/index.tsx
+++ front/src/components/_ui/TableComponent/index.tsx
@@ -9,59 +9,57 @@ interface Props {
 
 export function TableComponent({ columns, rows, loading }: Props) {
   return (
-    <>
-      <table style={loading ? { opacity: 0.5 } : {}} className={style.table}>
-        <thead>
+    <table style={loading ? { opacity: 0.5 } : {}} className={style.table}>
+      <thead>
+        <tr>
+          {columns?.map((column) => {
+            return (
+              <th key={column.field}>
+                <p>{column?.headerName || ''}</p>
+              </th>
+            )
+          })}
+        </tr>
+      </thead>
+      <tbody>
+        {rows.length > 0 &&
+          rows?.map((row) => {
+            return (
+              <tr key={row._id}>
+                {columns.map((column) => {
+                  return (
+                    <td
+                      className={column?.cellClass?.({
+                        value: row[column.field],
+                        data: row,
+                      })}
+                      key={column.field}
+                      style={{ flex: 1 }}
+                    >
+                      {column?.valueFormatter?.({
+                        value: row[column.field],
+                        data: row,
+                      })}
+
+                      {column?.cellRenderer?.({
+                        value: row[column.field],
+                        data: row,
+                      })}
+                    </td>
+                  )
+                })}
+              </tr>
+            )
+          })}
+
+        {rows.length === 0 && !loading && (
           <tr>
-            {columns?.map((column) => {
-              return (
-                <th key={column.field}>
-                  <p>{column?.headerName || ''}</p>
-                </th>
-              )
-            })}
+            <td className={style.emptyCell} colSpan={columns.length}>
+              <p>Nenhum item encontrado</p>
+            </td>
           </tr>
-        </thead>
-        <tbody>
-          {rows.length > 0 &&
-            rows?.map((row) => {
-              return (
-                <tr key={row._id}>
-                  {columns.map((column) => {
-                    return (
-                      <td
-                        className={column?.cellClass?.({
-                          value: row[column.field],
-                          data: row,
-                        })}
-                        key={column.field}
-                        style={{ flex: 1 }}
-                      >
-                        {column?.valueFormatter?.({
-                          value: row[column.field],
-                          data: row,
-                        })}
-
-                        {column?.cellRenderer?.({
-                          value: row[column.field],
-                          data: row,
-                        })}
-                      </td>
-                    )
-                  })}
-                </tr>
-              )
-            })}
-
-          {rows.length === 0 && !loading && (
-            <tr>
-              <td className={style.emptyCell} colSpan={columns.length}>
-                <p>Nenhum item encontrado</p>
-              </td>
-            </tr>
-          )}
-        </tbody>
-      </table>
-    </>
+        )}
+      </tbody>
+    </table>
   )
 }
