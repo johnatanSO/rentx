d318c7b1423438ad0ddf36bcc7711f93fb3cda0b
“Johnatan <jonystrit@gmail.com>
2024-01-14 22:24:52 -0300

    :sparkles: [FEAT] - Criando funcionalidade para deletar um carro.

--- front/src/components/screens/Management/CarsManagement/CarInfos/index.tsx
+++ front/src/components/screens/Management/CarsManagement/CarInfos/index.tsx
@@ -3,18 +3,61 @@
 import { Car } from './interfaces/Car'
 import style from './CarInfos.module.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAngleLeft } from '@fortawesome/free-solid-svg-icons'
+import { faAngleLeft, faTrash } from '@fortawesome/free-solid-svg-icons'
 import { useRouter } from 'next/navigation'
 import { ImagesSection } from './partials/ImagesSection'
 import { SpecificationsSection } from './partials/SpecificationsSection'
 import { EditInfosSection } from './partials/EditInfosSection'
+import { useContext } from 'react'
+import { AlertContext } from '@/contexts/alertContext'
+import { deleteCarService } from '@/services/cars/deleteCar/DeleteCarService'
 
 type Props = {
   car: Car
 }
 
 export function CarInfos({ car }: Props) {
+  const {
+    setAlertConfirmConfigs,
+    alertConfirmConfigs,
+    setAlertNotifyConfigs,
+    alertNotifyConfigs,
+  } = useContext(AlertContext)
   const router = useRouter()
+
+  function handleDeleteCar() {
+    const carId = car._id.toString()
+
+    setAlertConfirmConfigs({
+      ...alertConfirmConfigs,
+      open: true,
+      text: 'Deseja mesmo deletar este carro? Após a confirmação, essa ação será irreversível',
+      title: 'Alerta de confirmação',
+      onClickAgree: async () => {
+        deleteCarService(carId)
+          .then(() => {
+            setAlertNotifyConfigs({
+              ...alertNotifyConfigs,
+              open: true,
+              text: 'Carro deletado com sucesso',
+              type: 'success',
+            })
+
+            router.back()
+          })
+          .catch((error) => {
+            setAlertNotifyConfigs({
+              ...alertNotifyConfigs,
+              open: true,
+              text: `Erro ao tentar deletar este carro - ${
+                error?.response?.data?.message || error?.message
+              }`,
+              type: 'error',
+            })
+          })
+      },
+    })
+  }
 
   return (
     <>
@@ -29,6 +72,14 @@ export function CarInfos({ car }: Props) {
         <h2>
           {car.name || 'Sem nome'} | {car.licensePlate}
         </h2>
+        <button
+          type="button"
+          className={style.removeButton}
+          onClick={handleDeleteCar}
+        >
+          <FontAwesomeIcon icon={faTrash} className={style.icon} />
+          Deletar carro
+        </button>
       </header>
 
       <ImagesSection car={car} />
---
+++ front/src/services/cars/deleteCar/DeleteCarService.ts
@@ -0,0 +1,5 @@
+import http from '@/http/axios'
+
+export function deleteCarService(carId: string) {
+  return http.delete('/cars/' + carId)
+}
