d2b813a4751112039985a1a3a09de19a221b3a0f
“Johnatan <jonystrit@gmail.com>
2024-01-20 17:09:00 -0300

    :sparkles: [FEAT] - Finalizando página de sobre.

--- front/src/components/screens/About/index.tsx
+++ front/src/components/screens/About/index.tsx
@@ -1,8 +1,15 @@
+'use client'
 import Link from 'next/link'
 import style from './About.module.scss'
-import { Divider } from '@mui/material'
+import { Divider, Popover } from '@mui/material'
+import { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faBriefcase } from '@fortawesome/free-solid-svg-icons'
 
 export function About() {
+  const [anchorEl, setAnchorEl] = useState<any>(null)
+
   return (
     <section className={style.aboutContainer}>
       <header>
@@ -12,13 +19,23 @@ export function About() {
       <section>
         <div className={style.aboutSystemInfos}>
           <p>
-            O <b>RentX</b> é uma aplicação completa destinada à um
-            estabelecimento que realiza alugueis e controle de veículos.
+            O{' '}
+            <Link target={'_blank'} href="https://github.com/johnatanSO/rentx">
+              RentX
+            </Link>{' '}
+            é uma aplicação completa destinada à um estabelecimento que realiza
+            alugueis e controle de veículos.
           </p>
 
           <p>
             Foi inteiramente construído pelo desenvolvedor{' '}
-            <button type="button" className={style.perfilLinksButton}>
+            <button
+              onClick={(event) => {
+                setAnchorEl(event?.currentTarget)
+              }}
+              type="button"
+              className={style.perfilLinksButton}
+            >
               Johnatan Santos
             </button>{' '}
             e não possui nenhum fim lucrativo, somente para portfólio e
@@ -47,8 +64,8 @@ export function About() {
             estão disponíveis o gerenciamento de carros cadastrados e edição de
             informações, controle de categorias e especificações, controle de
             todos os alugueis realizados por todos os usuários com opção de
-            edição e também a possibilidade de importação de categorias através
-            de um arquivo .CSV
+            edição e também a possibilidade de importação de novas categorias
+            através de um arquivo .CSV
           </p>
         </div>
 
@@ -95,12 +112,55 @@ export function About() {
 
             <li>Jest para testes unitários</li>
 
-            <li>Nodemailer para envio de e-mails</li>
+            <li>Nodemailer</li>
 
-            <li>JWT para gerenciamento de tokens</li>
+            <li>JWT</li>
+
+            <li>Firebase Storage</li>
           </ul>
         </div>
       </section>
+
+      <Popover
+        id="simple-popover"
+        open={!!anchorEl}
+        anchorEl={anchorEl}
+        onClose={() => {
+          setAnchorEl(null)
+        }}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        sx={{ borderRadius: 15 }}
+      >
+        <div className={style.popoverContainer}>
+          <Link
+            className={style.linkedinLink}
+            target={'_blank'}
+            href="https://www.linkedin.com/in/johnatan-santos/"
+          >
+            <FontAwesomeIcon icon={faLinkedin} />
+            Linkedin
+          </Link>
+          <Link
+            className={style.githubLink}
+            target={'_blank'}
+            href="https://github.com/johnatanSO"
+          >
+            <FontAwesomeIcon icon={faGithub} />
+            Github
+          </Link>
+          <Link
+            className={style.portfolioLink}
+            target={'_blank'}
+            href="https://portfolio-johnatanso.vercel.app/"
+          >
+            <FontAwesomeIcon icon={faBriefcase} />
+            Portfólio
+          </Link>
+        </div>
+      </Popover>
     </section>
   )
 }
