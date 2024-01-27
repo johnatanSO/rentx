cfb63c848115e62a9f9c0e1c4c21ab17fdc884fa
“Johnatan <jonystrit@gmail.com>
2024-01-27 18:02:06 -0300

    :bug: [FIX] - Corrigindo erro do deploy na vercel por conta do delay na primeira requisição.

--- front/src/components/screens/Home/index.tsx
+++ front/src/components/screens/Home/index.tsx
@@ -1,20 +1,43 @@
 'use client'
 
+import { getAvaliableCarsService } from '@/services/cars/getAvaliableCars/GetAvaliableCarsService'
 import { Filters } from './Filters'
 import style from './Home.module.scss'
 import { Car } from './interfaces/Car'
 import { ListCars } from '@/components/_ui/ListCars'
+import { useEffect, useState } from 'react'
 
-interface Props {
-  cars: Car[]
-}
+export function Home() {
+  const [cars, setCars] = useState<Car[]>([])
+  const [loadingGetCars, setLoadingGetCars] = useState<boolean>(true)
 
-export function Home({ cars }: Props) {
+  // Esta e outras funções estavam sendo executadas no SRR, porém, como estou
+  // utilizando o mongoDB atlas, ele tem um 'delay' de alguns segundos na
+  // primeira requisição, e aí por conta desse delay, ocorre um erro na hora
+  // do deploy na vercel.
+  function getAvaliableCars() {
+    setLoadingGetCars(true)
+    getAvaliableCarsService({ name: '', categoryId: '' })
+      .then(({ data: { items } }) => {
+        setCars(items)
+      })
+      .catch(() => {
+        console.log('Erro ao buscar carros disponíveis')
+      })
+      .finally(() => {
+        setLoadingGetCars(false)
+      })
+  }
+
+  useEffect(() => {
+    getAvaliableCars()
+  }, [])
+
   return (
     <div className={style.carsContainer}>
       <Filters />
 
-      <ListCars loading={false} cars={cars} />
+      <ListCars loading={loadingGetCars} cars={cars} />
     </div>
   )
 }
