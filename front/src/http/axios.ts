b1fab07e9008ccd57e8a248fab8ebe49f015c369
Johnatan <devsantosjohn@gmail.com>
2024-08-01 14:48:42 -0300

    :fire: [REMOVE] - Removendo importações desnecessárias.

--- front/src/http/axios.ts
+++ front/src/http/axios.ts
@@ -1,6 +1,6 @@
 // import { getRefreshToken } from '@/services/token/getRefreshToken/GetRefreshToken'
 import { getTokenService } from '@/services/token/getToken/GetTokenService'
-import axios, { AxiosRequestConfig, InternalAxiosRequestConfig } from 'axios'
+import axios from 'axios'
 
 const http = axios.create({
   baseURL: process.env.NEXT_PUBLIC_END_POINT,
