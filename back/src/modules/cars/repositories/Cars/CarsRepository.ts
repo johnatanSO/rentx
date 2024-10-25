83ca56c092f8913572fdbce9de831f405837f544
JohnatanSO <devsantosjohn@gmail.com>
2024-10-25 17:22:03 -0300

    red

--- back/src/modules/cars/repositories/Cars/CarsRepository.ts
+++ back/src/modules/cars/repositories/Cars/CarsRepository.ts
@@ -2,7 +2,7 @@ import { Model } from 'mongoose'
 import { Car, CarModel } from '../../infra/mongoose/entities/Car'
 import { ICarsRepository, ICreateNewCarDTO } from './ICarsRepository'
 
-// a
+// aa
 export class CarsRepository implements ICarsRepository {
   private model: Model<Car>
   constructor() {
