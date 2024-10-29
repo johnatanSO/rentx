d773b64cff529380fca080d1ed1d423af053dbeb
JohnatanSO <devsantosjohn@gmail.com>
2024-10-28 21:54:32 -0300

    run deploy

--- back/src/config/upload.ts
+++ back/src/config/upload.ts
@@ -11,6 +11,7 @@ export default {
     filename: (request, file, callback) => {
       const fileHash = crypto.randomBytes(16).toString('hex')
       const fileName = `${fileHash}-${file.originalname}`
+      // a
 
       return callback(null, fileName)
     },
